e3cee3d35411e1519271d26fa396a72ae7b84ccf
Hafiz Saqlain Ur Rehman <114049185+Hafizsaqlain@users.noreply.github.com>
2024-10-02 08:51:17 +0500

    models

---
+++ lib/models.dart/newsModle.dart
@@ -0,0 +1,24 @@
+class CategoryModel {
+  String? categoryName;
+
+  CategoryModel() {
+    this.categoryName;
+  }
+}
+
+class NewsModle {
+  String? title;
+  String? description;
+
+  String? urlToImage;
+  String? author;
+  String? content;
+
+  NewsModle({
+    this.title,
+    this.description,
+    this.urlToImage,
+    this.author,
+    this.content,
+  });
+}
